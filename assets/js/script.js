bbb7b41105c1703c4d934fbd478eb9d032f48717
GBusta <104409829+GBusta@users.noreply.github.com>
2022-09-08 04:18:41 -0500

    started color scheme template for css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,41 @@
 const startBtn = document.querySelector('#startBtn')
 const timer = document.querySelector('#timer')
-let time = 75;
-const questions = [{question: 'What is today?', answer: 'Thursday'}, {}]
+const introDiv = document.querySelector('#introDiv')
+const quizQuestions = document.querySelector('#quizQuestions')
+
+var time = 5;
+
+let questions = [
+    {
+    question: "When did COVID happen?",
+    choices: ["2019", "2020", "2022"],
+    answer: "2020"
+    }
+    ]
+
+// const questions = [{question: 'What is today?', answer: 'Thursday'}, {}]
 
 startBtn.addEventListener('click', function() {
-  //add timer
-    setInterval(function(){
-        time--
-        timer.textContent = time;
-    }, 1000)
+    setTime()
+    displayQuestion()
 })
+
+function setTime () {
+    var countdown = setInterval(function() {
+        time--;
+    if (time === 0) {
+        clearInterval(countdown)
+    }
+    timer.textContent=time
+    
+    }, 1000)
+}
+
+function displayQuestion() {
+    introDiv.classList.remove("show")
+    introDiv.classList.add('noshow')
+    quizQuestions.classList.remove("noshow")
+    quizQuestions.classList.add('show')
+    // startBtn.classList.remove("show")
+    // startBtn.classList.remove("noshow")
+}
